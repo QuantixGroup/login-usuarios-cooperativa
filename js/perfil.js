22e10cdbfbe9a82b6c7df4739bd4c3897dfeafbd
CrisLorenzo07 <sscristian77@gmail.com>
2025-09-24 19:46:42 -0300

    Se crea html y estilos para perfil del usuario

--- js/perfil.js
+++ js/perfil.js
@@ -1,12 +1,36 @@
-Dropzone.options.fotoDropzone = {
-  paramName: "foto",
-  maxFiles: 1,
-  acceptedFiles: "image/*",
-  headers: { Authorization: "Bearer " + sessionStorage.getItem("tokenAcceso") },
-  success: function (file, response) {
-    alert("Foto subida correctamente");
-  },
-  error: function (file, response) {
-    alert("Error al subir la foto");
-  },
-};
+document.addEventListener("DOMContentLoaded", function () {
+  var btnFoto = document.querySelector(".btn-foto");
+  var dropzoneFoto = document.getElementById("fotoDropzone");
+  var dz = null;
+  if (btnFoto && dropzoneFoto) {
+    dropzoneFoto.style.display = "none";
+    btnFoto.addEventListener("click", function () {
+      dropzoneFoto.style.display = "block";
+      if (!dz) {
+        dz = new Dropzone(dropzoneFoto, {
+          paramName: "foto",
+          maxFiles: 1,
+          acceptedFiles: "image/*",
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("tokenAcceso"),
+          },
+          success: function (file, response) {
+            if (response && response.url) {
+              var img = document.querySelector(".foto-img");
+              if (img) img.src = response.url;
+            }
+            alert("Foto subida correctamente");
+          },
+          error: function (file, response) {
+            alert("Error al subir la foto");
+          },
+          autoProcessQueue: true,
+        });
+      }
+      setTimeout(function () {
+        var dzInput = dropzoneFoto.querySelector("input[type='file']");
+        if (dzInput) dzInput.click();
+      }, 300);
+    });
+  }
+});
